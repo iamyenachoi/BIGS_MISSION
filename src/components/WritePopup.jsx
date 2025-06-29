c3d918ec381203c042c7e2b45922285f0e89adce
yenachoi <yeahna825@gmail.com>
2025-06-29 21:34:25 +0900

    final

--- src/components/WritePopup.jsx
+++ src/components/WritePopup.jsx
@@ -74,11 +74,7 @@ function WritePopup() {
                             <option value="NOTICE">공지사항</option>
                             <option value="FREE">자유게시판</option>
                             <option value="QUESTION">질문게시판</option>
-<<<<<<< HEAD
-    <option value="ETC">기타게시판</option>
-=======
                             <option value="QUESTION">기타게시판</option>
->>>>>>> 2f7d257dc862068ddf3f4a557f7a6a3cf99130bb
                         </select >
                     </div >
                     <div className="write-input-box">
